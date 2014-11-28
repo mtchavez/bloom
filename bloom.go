3162d1153f3131f6926ac51fcab31593deae557a
Chavez <matthew@el-chavez.me>
2014-11-28 10:36:51 -0700

    Optimal K

--- bloom.go
+++ bloom.go
@@ -3,6 +3,7 @@ package bloom
 import (
 	"hash"
 	"hash/fnv"
+	"math"
 
 	"github.com/mtchavez/bitset"
 )
@@ -21,4 +22,11 @@ func NewFilter(k, m uint64) *Filter {
 		f: bitset.New(m),
 		h: fnv.New64(),
 	}
+}
+
+func OptimalK(m, n uint64) uint64 {
+	if n <= 0 {
+		return 0
+	}
+	return uint64(math.Ceil((float64(m) / float64(n)) * math.Log(2)))
 }
--- bloom_test.go
+++ bloom_test.go
@@ -15,3 +15,22 @@ func Test_NewFilter(t *testing.T) {
 		t.Errorf("Expected m to be set to %+v but got %+v", m, f.m)
 	}
 }
+
+func Test_OptimalK_ZeroN(t *testing.T) {
+	k := OptimalK(1000, 0)
+	if k != 0 {
+		t.Errorf("Expected k to be zero if n is <= 0")
+	}
+}
+
+func Test_OptimalK(t *testing.T) {
+	k := OptimalK(1e9, 1e6)
+	if k != 694 {
+		t.Errorf("Expected k to be 694 but got %+v", k)
+	}
+
+	k = OptimalK(1e12, 1e11)
+	if k != 7 {
+		t.Errorf("Expected k to be 7 but got %+v", k)
+	}
+}
